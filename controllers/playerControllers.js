5ab7de765e42730aa8fac81398c69fcf5b99140d
Shibin C Reji <scr2404@gmail.com>
2023-01-28 20:24:18 +0530

    Update playerControllers.js

--- controllers/playerControllers.js
+++ controllers/playerControllers.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';111111111
+import mongoose from 'mongoose';1111111111
 import { PlayerSchema } from '../models/playerModel';
 
 const Player = mongoose.model('Player', PlayerSchema);
